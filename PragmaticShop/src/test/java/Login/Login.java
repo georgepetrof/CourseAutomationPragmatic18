5815fed883820de0bdcfcbced70293760b185fe1
Your Name <you@example.com>
2018-10-31 20:04:18 +0200

    new branch

--- PragmaticShop/src/test/java/Login/Login.java
+++ PragmaticShop/src/test/java/Login/Login.java
@@ -1,5 +1,10 @@
 package Login;
 
+import static org.testng.Assert.assertEquals;
+
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import javax.lang.model.element.Element;
@@ -9,11 +14,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.ClickAction;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import okio.Options;
 
 public class Login {
 
@@ -43,6 +52,10 @@ public class Login {
 		WebElement loginButton = this.driver.findElement(By.cssSelector("div button"));
 		loginButton.click();
 
+		WebElement element = this.driver.findElement(By.cssSelector(".container-fluid > h1"));
+		String dashboard = element.getText();
+		Assert.assertEquals("Dashboard", dashboard);
+
 	}
 
 	@Test
@@ -55,15 +68,37 @@ public class Login {
 
 		WebElement loginButton = this.driver.findElement(By.cssSelector("div button"));
 		loginButton.click();
+
+		WebElement loginFail = this.driver.findElement(By.cssSelector(".alert .fa "));
+		String text = loginFail.getAttribute("class");
+
+		Assert.assertEquals(text, "fa fa-exclamation-circle");
+
 	}
 
 	@Test
-	public void orderStatus() {
+	public void orderStataus() {
 		canLogIn();
 		WebElement sales = this.driver.findElement(By.cssSelector("#menu a[href*=\"collapse26\"]"));
 		sales.click();
 		WebElement orders = this.driver.findElement(By.cssSelector("#collapse26 a[href*=\"order\"]"));
 		orders.click();
-		Select orderStatus = new Select(this.driver.findElement(By.name("Order Status")));
+
+		WebElement dropDown = this.driver.findElement(By.cssSelector("#input-order-status"));
+		Select orderStatus = new Select(dropDown);
+		java.util.List<WebElement> act_options = orderStatus.getOptions();
+		
+		
+		
+		java.util.List<String> exp_options = Arrays.asList(new String[] {"Missing Orders", "Canceled",
+				"Canceled Reversal", "Chargeback", "Complete", "Denied", "Expired", "Failed", "Pending", "Processed",
+				"Processing", "Refunded", "Reversed", "Shipped", "Voided" });
+
+	
+
+		assertEquals(act_options.toString(), exp_options.toString());
+		
+
 	}
+
 }
